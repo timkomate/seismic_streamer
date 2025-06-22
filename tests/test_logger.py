a0e25a6d3929e117c53b2216bd3a980f1a1699aa
Mate Timko <timko.mate@gmail.com>
2025-06-22 11:22:25 +0200

    Fix logging test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,25 +1,11 @@
-import logging
-
-import pytest
+from unittest.mock import patch
 
 from seismic_streamer.logging_config import setup_logging
 
 
-def test_setup_logging_sets_info_level(caplog):
-    caplog.set_level("INFO")
-    logger = logging.getLogger()
-    logger.info("Test info log")
-    logger.debug("Test debug log")
+def test_setup_logging_calls_basicconfig():
+    with patch("logging.basicConfig") as mock_basic:
+        from seismic_streamer.logging_config import setup_logging
 
-    assert any("Test info log" in m for m in caplog.messages)
-    assert not any("Test debug log" in m for m in caplog.messages)
-
-
-def test_setup_logging_sets_debug_level(caplog):
-    caplog.set_level("DEBUG")
-    logger = logging.getLogger()
-    logger.info("Test info log")
-    logger.debug("Test debug log")
-
-    assert any("Test info log" in m for m in caplog.messages)
-    assert any("Test debug log" in m for m in caplog.messages)
+        setup_logging("INFO")
+        mock_basic.assert_called_once()
